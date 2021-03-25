269eac1a8ac20029f169e1cbd0223028e8b7788a
Marco Emrich <marco.emrich@codecentric.de>
2021-03-25 08:44:13 +0100

    improve player story

--- src/components/Player.stories.js
+++ src/components/Player.stories.js
@@ -10,11 +10,13 @@ export default {
   },
 };
 
-export const Template = ((args) => {
+const Template = ((args) => {
   const [name, setName] = React.useState("Bob");
   return <Player label={args.label} onChange={(e) => setName(e.target.value)} name={name} />;
 }).bind({});
 
-Template.args = {
+export const Default = Template.bind({});
+
+Default.args = {
   label: "X",
 };
